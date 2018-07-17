5f2e6ec3ce53ebce3e7c6448d0d5f77479dfa119
Mark Baker <mrbaker1917@gmail.com>
2018-07-17 16:43:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,10 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, el) {
   var newArray = [el, ...arr];
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, el) {
   arr = [el, ...arr];
+  return arr;
 }
